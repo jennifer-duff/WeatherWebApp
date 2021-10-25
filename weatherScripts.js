a8c837144e5e206a2f1dbfe701cfd307b80da4ac
Jennifer Duff <jduffbowser@gmail.com>
2021-10-25 11:16:06 -0700

    Added JS functionality to top section

--- weatherScripts.js
+++ weatherScripts.js
@@ -1,16 +1,15 @@
 'use strict'
 
+// -------------------------------------- Carousel functionality --------------------------------------
 let prevArrow = document.querySelector('#leftArrow');
 let nextArrow = document.querySelector('#rightArrow');
 let forecastBox = document.querySelector('#nowBox');
 let carousel = document.querySelector('#carousel');
 
-// let scrollWidth = forecastBox.clientWidth + forecastBox.getComputedStyle();
 let forecastBoxStyle = window.getComputedStyle(forecastBox);
 let forecastBoxWidth = 2 * parseFloat(forecastBoxStyle.marginRight) + parseFloat(forecastBoxStyle.width)
 
 let forecastBoxHeight = parseFloat(forecastBoxStyle.height);
-console.log(forecastBoxHeight);
 
 function scrollToNext(){
     carousel.scrollBy({
@@ -29,3 +28,91 @@ prevArrow.addEventListener('click', scrollToPrev);
 
 carousel.style.width = forecastBoxWidth * 4 + 100;
 carousel.style.height = forecastBoxHeight + 50;
+
+
+//--------------------------------------- Get location --------------------------------------
+let locationInputElement = document.querySelector('#locationInput');
+let strUserLocation = 'userLocation';
+let currTemp = document.querySelector('#currTemp');
+let currCondition = document.querySelector('#currCondition');
+let highTemp = document.querySelector('#highTemp');
+let lowTemp = document.querySelector('#lowTemp');
+
+
+async function getLocationKey(location) {
+    let response = await axios.get(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=mZDDGnloK5jU8t1fbOA952AYshZ4mJYN&q=${location}`)
+    console.log(response.data[0].Key);
+    return response.data[0].Key;
+}
+
+async function getCurrTemp(location){
+    let key = await getLocationKey(location);
+    await axios.get(`http://dataservice.accuweather.com/currentconditions/v1/${key}?apikey=mZDDGnloK5jU8t1fbOA952AYshZ4mJYN`)
+        .then(function (response) {
+            let temp = response.data[0].Temperature.Imperial.Value;
+            currTemp.innerText = temp;
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+}
+
+async function getCurrCondition(location){
+    let key = await getLocationKey(location);
+    await axios.get(`http://dataservice.accuweather.com/currentconditions/v1/${key}?apikey=mZDDGnloK5jU8t1fbOA952AYshZ4mJYN`)
+        .then(function (response) {
+            let condition = response.data[0].WeatherText;
+            currCondition.innerText = condition;
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+}
+
+async function getHighTemp(location){
+    let key = await getLocationKey(location);
+    await axios.get(`http://dataservice.accuweather.com/forecasts/v1/daily/1day/${key}?apikey=mZDDGnloK5jU8t1fbOA952AYshZ4mJYN`)
+    .then(function (response) {
+        console.log(response);
+        let htemp = response.data.DailyForecasts[0].Temperature.Maximum.Value;
+        highTemp.innerText = htemp;
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+}
+
+async function getLowTemp(location){
+    let key = await getLocationKey(location);
+    await axios.get(`http://dataservice.accuweather.com/forecasts/v1/daily/1day/${key}?apikey=mZDDGnloK5jU8t1fbOA952AYshZ4mJYN`)
+    .then(function (response) {
+        console.log(response);
+        let ltemp = response.data.DailyForecasts[0].Temperature.Minimum.Value;
+        lowTemp.innerText = ltemp;
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+}
+
+
+locationInputElement.addEventListener('change', function() {
+    if (locationInputElement.value === null || locationInputElement.value === '')
+    {
+        strUserLocation = 'No location entered';
+    }
+    else
+    {
+        strUserLocation = locationInputElement.value;
+        console.log(strUserLocation);
+        getCurrTemp(strUserLocation);
+        getCurrCondition(strUserLocation);
+        getHighTemp(strUserLocation);
+        getLowTemp(strUserLocation);
+    }
+})
+
+
+
+    
+
