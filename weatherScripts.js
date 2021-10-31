56ac02b985714a91f9d99889eec8eb625ae39c22
Jennifer Duff <jduffbowser@gmail.com>
2021-10-30 22:13:44 -0700

    Update carousel functionality to include clickable scroll indicator/navigation

--- weatherScripts.js
+++ weatherScripts.js
@@ -8,19 +8,22 @@ let carousel = document.querySelector('#carousel');
 
 let forecastBoxStyle = window.getComputedStyle(forecastBox);
 let forecastBoxWidth = 2 * parseFloat(forecastBoxStyle.marginRight) + parseFloat(forecastBoxStyle.width)
-
 let forecastBoxHeight = parseFloat(forecastBoxStyle.height);
+
+let clickCounter = 0;
 
 function scrollToNext(){
     carousel.scrollBy({
         left: forecastBoxWidth, top: 0, behavior: 'smooth'
     });
+    clickCounter ++;
 }
 
 function scrollToPrev(){
     carousel.scrollBy({
         left: -forecastBoxWidth, top: 0, behavior: 'smooth'
     });
+    clickCounter --;
 }
 
 nextArrow.addEventListener('click', scrollToNext);
@@ -29,8 +32,138 @@ prevArrow.addEventListener('click', scrollToPrev);
 carousel.style.width = forecastBoxWidth * 4 + 100;
 carousel.style.height = forecastBoxHeight + 50;
 
+// -------------------------- Carousel circle buttons ------------------------
+let dotNavs = document.querySelectorAll('.dotNav');
+let firstDot = document.querySelector('#firstDot');
+let secondDot = document.querySelector('#secondDot');
+let thirdDot = document.querySelector('#thirdDot');
 
-//--------------------------------------- Get location --------------------------------------
+firstDot.classList.add('dotNavActive');
+
+//Dynamically set activeDot
+function setActiveDot(){
+    if (clickCounter < 4)
+    {
+        firstDot.classList.add('dotNavActive');
+        secondDot.classList.remove('dotNavActive');
+        thirdDot.classList.remove('dotNavActive');
+    }
+    else if (clickCounter >= 4 && clickCounter < 8)
+    {
+        firstDot.classList.remove('dotNavActive');
+        secondDot.classList.add('dotNavActive');
+        thirdDot.classList.remove('dotNavActive');
+    }
+    else if (clickCounter >= 8)
+    {
+        firstDot.classList.remove('dotNavActive');
+        secondDot.classList.remove('dotNavActive');
+        thirdDot.classList.add('dotNavActive');
+    }
+}
+
+nextArrow.addEventListener('click', setActiveDot);
+prevArrow.addEventListener('click', setActiveDot);
+
+
+//Allow for clicking of dots to navigate
+function findActiveDot(){
+    for (let i = 0; i <= dotNavs.length; i++)
+    {
+        if (dotNavs[i].classList.contains('dotNavActive'))
+        {
+            return (i + 1);
+        }
+    }
+}
+
+function scrollForwardByFour(){
+    carousel.scrollBy({
+        left: (4 * forecastBoxWidth), top: 0, behavior: 'smooth'
+    });
+    clickCounter = clickCounter + 4;
+}
+
+function scrollForwardByEight(){
+    carousel.scrollBy({
+        left: (8 * forecastBoxWidth), top: 0, behavior: 'smooth'
+    });
+    clickCounter = clickCounter + 8;
+}
+
+function scrollBackByFour(){
+    carousel.scrollBy({
+        left: -(4 * forecastBoxWidth), top: 0, behavior: 'smooth'
+    });
+    clickCounter = clickCounter - 4;
+}
+
+function scrollBackByEight(){
+    carousel.scrollBy({
+        left: -(8 * forecastBoxWidth), top: 0, behavior: 'smooth'
+    });
+    clickCounter = clickCounter - 8;
+}
+
+function scroll(){
+    let activeDotNum = findActiveDot();
+    console.log(`activeDotNum: ${activeDotNum}`);
+    let activeDotElement = dotNavs[activeDotNum - 1];
+    let nextDotID = this.id;
+    let nextDotElement = document.querySelector(`#${nextDotID}`);
+    let nextDotNum = 0;
+
+    //convert goToDot to an int
+    if (nextDotID === 'firstDot')
+    {
+        nextDotNum = 1;
+    }
+    else if (nextDotID === 'secondDot')
+    {
+        nextDotNum = 2;
+    }
+    else if (nextDotID === 'thirdDot')
+    {
+        nextDotNum = 3;
+    }
+    console.log(`nextDot: ${nextDotNum}`);
+
+    if ((nextDotNum - activeDotNum) === 1 )
+    {
+        console.log('scrolling forward')
+        scrollForwardByFour();
+        activeDotElement.classList.toggle('dotNavActive');
+        nextDotElement.classList.add('dotNavActive');
+    }
+    else if ((nextDotNum - activeDotNum) === 2 )
+    {
+        console.log('scrolling forward twice!')
+        scrollForwardByEight();
+        activeDotElement.classList.toggle('dotNavActive');
+        nextDotElement.classList.add('dotNavActive');
+    
+    }
+    else if ((nextDotNum - activeDotNum) === -1)
+    {
+        console.log('scrolling backwards')
+        scrollBackByFour();
+        activeDotElement.classList.toggle('dotNavActive');
+        nextDotElement.classList.add('dotNavActive');
+    }
+    else if ((nextDotNum - activeDotNum) === -2 )
+    {
+        console.log('scrolling backs twice!')
+        scrollBackByEight();
+        activeDotElement.classList.toggle('dotNavActive');
+        nextDotElement.classList.add('dotNavActive');
+    }
+}
+
+for (let i = 0; i < dotNavs.length; i++){
+    dotNavs[i].addEventListener('click', scroll);
+}
+
+//--------------------- Get location + Set Conditions ------------------------
 let locationInputElement = document.querySelector('#locationInput');
 let strUserLocation = 'userLocation';
 let currTemp = document.querySelector('#currTemp');
@@ -111,6 +244,8 @@ locationInputElement.addEventListener('change', function() {
         getLowTemp(strUserLocation);
     }
 })
+
+
 
 
 
